fded04e0d4e1c08abead4695fc45758ee7d7cb99
JaxonsherZievudinov <zievudinovdzaha@gmail.com>
2024-12-07 16:37:22 +0500

    first commit

---
+++ add.js
@@ -0,0 +1,61 @@
+// let a=100;
+// let b=10;
+// let c=30;
+
+// let result = (a - b > b - c && c - a) || (b - c > a - b && c - a) || (c - a > b - c && b - c);
+
+// console.log(a-b>b-c&&c-a?a-b:b-c>a-b&&c-a?b-c:c-a);
+
+// console.log(result);
+
+
+// function sum(a){
+//     if(a<0){
+//         a=a-a-a
+//         if(a%10==9){
+//             return true
+//         }
+//     }
+//     else if (a%10==9){
+//         return true
+//     }
+//     else return false
+//  }
+//  console.log(sum(141));
+
+function sum(a) {
+    if(a%10==9) return true
+    else return false
+}
+console.log(sum(1490));
+
+
+
+//  let a =-150
+//  console.log(a-a-a);
+ 
+
+//  function sum(a){
+//     if(a%100==0){
+//         return Math.floor(a/100)
+//     }
+//     else{
+//         return Math.floor(a/100)+1
+//     }
+//  }
+//  console.log(sum(1555));
+
+
+// function sum(a){
+//     let str = ""
+//     for(let i=1;i<=10;i++){
+//        str+=i*a
+//        if(i!=10){
+//           str+=","
+//        }
+//     }
+//     return str
+//  }
+//  console.log(sum(5));
+ 
+ 
